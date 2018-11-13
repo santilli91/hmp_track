df19b5cc14cb62b8cc3ad29dba3835cbba58a708
Michael Santilli <santilli.mike.a@gmail.com>
2018-11-13 20:16:19 +0000

    Updated Roadblock section. Saving new takes you to new submissions.  Updating now saves submission

--- src/Form/RoadBlockForm.php
+++ src/Form/RoadBlockForm.php
@@ -6,6 +6,7 @@ use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Component\Utility\UrlHelper;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Drupal\Core\Routing\TrustedRedirectResponse;
 
 class RoadBlockForm extends FormBase {
 	public function buildForm(array $form, FormStateInterface $form_state, $aid = null) {
@@ -59,19 +60,29 @@ class RoadBlockForm extends FormBase {
 
 	public function submitForm(array &$form, FormStateInterface $form_state) {
 		$sub = array(
+			'id' => $form_state->getValue('aid'),
 			'name' => $form_state->getValue('name'),
-			'id' => $form_state->getValue('id'),
+			'ad_id' => $form_state->getValue('id'),
 			'enabled' => $form_state->getValue('enabled'),
 		);
-		if($form_state->getValue('aid') == 'new') {
+		if($sub['id'] == 'new') {
 			$query = \Drupal::database()->insert('hmp_track_ads')
 			->fields(array(
-				'name' => $form_state->getValue('name'),
-				'ad_id' => $form_state->getValue('id'),
-				'enabled' => $form_state->getValue('enabled'),
+				'name' => $sub['name'],
+				'ad_id' => $sub['ad_id'],
+				'enabled' => $sub['enabled']
 			))->execute();
+			$id = \Drupal::database()->select('hmp_track_ads','a')->fields('a',['id'])->orderBy('a.id','DESC')->range(0,1)->execute()->fetchField();
+			drupal_set_message('Created!');
+			$response =  new TrustedRedirectResponse('/admin/content/roadblock/' . $id);
+			$response->send();
 		} else {
-
+			$query = \Drupal::database()->update('hmp_track_ads')->fields(array(
+				'name' => $sub['name'],
+				'ad_id' => $sub['ad_id'],
+				'enabled' => $sub['enabled']
+			))->condition('id',$sub['id'])->execute();
+			drupal_set_message('Updated!');
 		}
 
 	}
