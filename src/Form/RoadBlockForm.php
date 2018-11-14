61cac10ae02aeb13e8617db62d6879c0c58dbc73
Michael Santilli <santilli.mike.a@gmail.com>
2018-11-14 16:33:49 +0000

    Added ability to create/update URIs for Roadblocks

--- src/Form/RoadBlockForm.php
+++ src/Form/RoadBlockForm.php
@@ -51,6 +51,11 @@ class RoadBlockForm extends FormBase {
 			'#title' => 'Enabled',
 			'#default_value' => isset($values['enabled'])?$values['enabled']:'',
 		);
+		$form['uri'] = array(
+			'#type' => 'textarea',
+			'#title' => 'URI (one per line):',
+			'#default_value' => isset($values['uri'])?$values['uri']:'',
+		);
 		$form['delete'] = $delete;
 		/** Submit Button **/
 		$form['break'] = array(
@@ -76,7 +81,9 @@ class RoadBlockForm extends FormBase {
 			'name' => $form_state->getValue('name'),
 			'ad_id' => $form_state->getValue('id'),
 			'enabled' => $form_state->getValue('enabled'),
+			'uris' => explode(PHP_EOL,$form_state->getValue('uri'))
 		);
+		/** If there is a new item **/
 		if($sub['id'] == 'new') {
 			$query = \Drupal::database()->insert('hmp_track_ads')
 			->fields(array(
@@ -85,26 +92,45 @@ class RoadBlockForm extends FormBase {
 				'enabled' => $sub['enabled']
 			))->execute();
 			$id = \Drupal::database()->select('hmp_track_ads','a')->fields('a',['id'])->orderBy('a.id','DESC')->range(0,1)->execute()->fetchField();
+			$this->insertURI($sub['uris'],$id);
 			drupal_set_message('Created!');
 			$response =  new TrustedRedirectResponse('/admin/content/roadblock/' . $id);
 			$response->send();
-		} else {
+		} 
+		/** Updating items **/
+		else {
+			/** Delete a roadblock and all of its data **/
 			if($form_state->getValue('delete') == 1) {
-				$query = \Drupal::database()->delete('hmp_track_ads')->condition('id',$sub['id'])->execute();
+				$query = \Drupal::database()->delete('hmp_track_ads')->condition('id',$sub['id'],'=')->execute();
+				$query = \Drupal::database()->delete('hmp_track_ads_uri')->condition('aid',$sub['id'],'=')->execute();
 				drupal_set_message('Roadblock: ' . $sub['name'] . ' has been deleted');
 				$response =  new TrustedRedirectResponse('/admin/content/roadblock/');
 				$response->send();
-			} else {
+			} 
+			/** Update item **/
+			else {
 				$query = \Drupal::database()->update('hmp_track_ads')->fields(array(
 					'name' => $sub['name'],
 					'ad_id' => $sub['ad_id'],
 					'enabled' => $sub['enabled']
 				))->condition('id',$sub['id'])->execute();
+				$this->insertURI($sub['uris'],$sub['id']);
 				drupal_set_message('Updated!');
 			}
 		}
 
 	}
+
+	private function insertURI($uris,$id) {
+		$query = \Drupal::database()->delete('hmp_track_ads_uri')->condition('aid',$id,'=')->execute();
+		foreach($uris as $uri) {
+			$query = \Drupal::database()->insert('hmp_track_ads_uri')
+				->fields(array(
+					'uri' => $uri,
+					'aid' => $id
+				))->execute();
+		}
+	} 
 
 	private function getDefaults($aid) {
 		$query = \Drupal::database()->select('hmp_track_ads','a');
@@ -119,6 +145,15 @@ class RoadBlockForm extends FormBase {
 				'enabled' => $result->enabled,
 			);
 		}
+		$query = \Drupal::database()->select('hmp_track_ads_uri','u');
+		$query->fields('u');
+		$query->condition('u.aid',$aid,'=');
+		$results = $query->execute();
+		$uris = '';
+		foreach($results as $uri) {
+			$uris .= $uri->uri . PHP_EOL;
+		}
+		$data['uri'] = $uris;
 		return $data;
 	}
 }
