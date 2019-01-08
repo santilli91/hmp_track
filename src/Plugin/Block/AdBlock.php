8fc7e1bb625c0a001806d343352552de0e507da2
Michael Santilli <santilli.mike.a@gmail.com>
2019-01-08 20:23:55 +0000

    added custom block for ad code.  Does not work with big pipe if HTML ad is selected for blocks

---
+++ src/Plugin/Block/AdBlock.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Drupal\hmp_track\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use \Drupal\node\Entity\Node;
+use Drupal\Core\Block\BlockPluginInterface;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Block that produces a list of all parent categories, given context, also lists current parent category's children.
+ *
+ * @Block(
+ *   id = "hmp_ad_block",
+ *   admin_label = @Translation("Ad Position"),
+ * )
+ */
+class AdBlock extends BlockBase implements BlockPluginInterface {
+
+  /**
+ * {@inheritdoc}
+ */
+public function blockForm($form, FormStateInterface $form_state) {
+  $form['zid'] = array(
+    '#type' => 'textfield',
+    '#title' => $this->t('ZID'),
+    '#description' => $this->t('Zone ID'),
+    '#default_value' => isset($this->configuration['zid']) ? $this->configuration['zid'] : '',
+    '#maxlength' => 12,
+    '#size' => 12,
+    '#weight' => '0',
+    '#required' => true,
+  );
+  $form['ad_type'] = array(
+    '#type' => 'select',
+    '#title' => $this->t('Ad Type'),
+    '#description' => $this->t('Ad Type - AJAX / HTML'),
+    '#default_value' => isset($this->configuration['ad_type']) ? $this->configuration['ad_type'] : '',
+    '#options' => array(
+      'ajax' => 'AJAX',
+      'html' => 'HTML',
+    ),
+    '#multiple' => false,
+    '#required' => true,
+  );
+  $form['ad_size'] = array(
+    '#type' => 'select',
+    '#title' => $this->t('Ad Size'),
+    '#description' => $this->t('Ad Size - Used for Proclivity'),
+    '#default_value' => isset($this->configuration['ad_size']) ? $this->configuration['ad_size'] : '',
+    '#options' => array(
+      '0' => 'N/A',
+      '300x250' => '300x250',
+      '728x90' => '728x90',
+    ),
+    '#multiple' => false,
+    '#required' => true,
+  );
+
+  return $form;
+}
+
+/**
+ * {@inheritdoc}
+ */
+public function blockSubmit($form, FormStateInterface $form_state) {
+  $this->configuration['zid'] = $form_state->getValue('zid');
+  $this->configuration['ad_size'] = $form_state->getValue('ad_size');
+  $this->configuration['ad_type'] = $form_state->getValue('ad_type');
+}
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $config = $this->getConfiguration();
+    $output = '';
+    if (!empty($config['zid'])) {
+      $output = hmp_track_get_adcode($config['zid'],$config['ad_type'],$config['ad_size']);
+    }
+    return array(
+      '#markup' => $this->t($output),
+      '#cache' => [
+      	'max-age' => 0,
+      ],
+    );
+  }
+
+/* */
+}?>
