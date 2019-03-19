c61d1be701c56ea122d24a7a4bde733be1370654
santilli91 <santilli.mike.a@gmail.com>
2019-03-19 13:02:47 +0000

    Added Support for Auth0 Variables through Woopra

--- js/hmp_track.js
+++ js/hmp_track.js
@@ -1,20 +1,20 @@
 var _avp = _avp || [];
 var _hmpHasRun = _hmpHasRun || 0;
 Drupal.behaviors.hmp_track = {
-	attach: function (context, settings) {
+  attach: function (context, settings) {
     if(_hmpHasRun == 0) {
       loadAdvertAVP(settings.hmp_track.config['advertserve_url'],settings.hmp_track.config['advertserve_timeout']);
-  	  loadDMD(settings.hmp_track.config['dmd_id']);
+      loadDMD(settings.hmp_track.config['dmd_id']);
       proclivityPX(settings.hmp_track);
       woopraScripts(settings.hmp_track);
       _hmpHasRun = 1;
     }
-	}
+  }
 };
 
 function loadDMD(dmd_id) {
   if(dmd_id != '') {
-  	(function(w,d,s,m,n,t){
+    (function(w,d,s,m,n,t){
       w[m]=w[m]||{init:function(){(w[m].q=w[m].q||[]).push(arguments);},ready:function(c){if('function'!=typeof c){return;}n.onload=n.onreadystatechange=function(){
       if(!n.readyState||/loaded|complete/.test(n.readyState)){n.onload=n.onreadystatechange=null;if(t.parentNode&&n.parentNode){t.parentNode.removeChild(n);}if(c){c();}}};}},
       w[m].d=1*new Date();n=d.createElement(s);t=d.getElementsByTagName(s)[0];n.async=1;n.src='//www.medtargetsystem.com/javascript/beacon.js?v2.5.11';
@@ -82,6 +82,14 @@ function woopraScripts(config) {
     if(config.npi != '') {
       woopra.identify({
         npi: config.npi,
+      })
+    }
+    if(config.auth0) {
+      woopra.identify({
+        first_name: config.auth0.first_name,
+        last_name: config.auth0.last_name,
+        degree: config.auth0.degree,
+        specialty: config.auth0.specialty
       })
     }
 
