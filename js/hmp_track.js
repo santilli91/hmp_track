5c6acb8781f64d99bc4a2f0f6b46a723ed10cd8f
santilli91 <santilli.mike.a@gmail.com>
2019-04-23 17:46:41 +0000

    Added ping interval to woopra

--- js/hmp_track.js
+++ js/hmp_track.js
@@ -71,7 +71,8 @@ function woopraScripts(config) {
           var t,i,e,n=window,o=document,a=arguments,s="script",r=["config","track","identify","visit","push","call","trackForm","trackClick"],c=function(){var t,i=this;for(i._e=[],t=0;r.length>t;t++)(function(t){i[t]=function(){return i._e.push([t].concat(Array.prototype.slice.call(arguments,0))),i}})(r[t])};for(n._w=n._w||{},t=0;a.length>t;t++)n._w[a[t]]=n[a[t]]=n[a[t]]||new c;i=o.createElement(s),i.async=1,i.src="//static.woopra.com/js/w.js",e=o.getElementsByTagName(s)[0],e.parentNode.insertBefore(i,e)
     })("woopra");
     woopra.config({
-        domain: config.config['woopra_id']
+        domain: config.config['woopra_id'],
+        ping_interval:5000
     });
     if(config.email) {
       woopra.identify({
